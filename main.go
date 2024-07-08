70331408cad726899d509833df9eabc793ffcaf2
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2024-07-08 23:41:07 +0900

    refactor: :fire: remove unnecessary comments

--- main.go
+++ main.go
@@ -14,38 +14,22 @@ func main() {
 	godotenv.Load()
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	tgbotapi.SetLogger(NewLogger())
-	bot.Debug = true
-
-	// Create a new UpdateConfig struct with an offset of 0. Offsets are used
-	// to make sure Telegram knows we've handled previous values and we don't
-	// need them repeated.
+	bot.Debug = true // Enable debug mode
 	updateConfig := tgbotapi.NewUpdate(0)
-
-	// Tell Telegram we should wait up to 30 seconds on each request for an
-	// update. This way we can get information just as quickly as making many
-	// frequent requests without having to send nearly as many.
 	updateConfig.Timeout = 30
-
-	// Start polling Telegram for updates.
 	updates := bot.GetUpdatesChan(updateConfig)
 
-	// Let's go through each update that we're getting from Telegram.
 	for update := range updates {
-		// Telegram can send many types of updates depending on what your Bot
-		// is up to. We only want to look at messages for now, so we can
-		// discard any other updates.
 		if update.Message == nil {
 			continue
 		}
 
 		// Take the Chat ID and Text from the incoming message
-		// and use it to create a new message.
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-		// We'll also say that this message is a reply to the previous message.
 		msg.ReplyToMessageID = update.Message.MessageID
 
 		// Check if the message *is* a valid URL and grab it
@@ -55,14 +39,8 @@ func main() {
 		if err != nil {
 			msg.Text += "Send a valid URL pls"
 		} else {
-
-			//Creates a Settings struct with default values, and save it to downloadMedia variable.
 			downloadMedia := gobalt.CreateDefaultSettings()
-
-			// Sets the URL, you MUST set one before downloading the media.
 			downloadMedia.Url = u.String()
-
-			// After changing the url, Run() will make the necessary requests to cobalt to download your media
 			result, err := gobalt.Run(downloadMedia)
 			if err != nil {
 				log.Error(err)
@@ -79,13 +57,8 @@ func main() {
 
 		}
 
-		// Okay, we're sending our message off! We don't care about the message
-		// we just sent, so we'll discard it.
 		if _, err := bot.Send(msg); err != nil {
-			// Note that panics are a bad way to handle errors. Telegram can
-			// have service outages or network errors, you should retry sending
-			// messages or more gracefully handle failures.
-			panic(err)
+			log.Fatal(err)
 		}
 	}
 }
