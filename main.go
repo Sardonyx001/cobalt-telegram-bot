4ff9ba651cffcb39a7471c1a69b670886bd81256
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2024-07-13 10:19:23 +0900

    feat: added inline keyboard callback handling to main bot

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	log "github.com/charmbracelet/log"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
-	"github.com/lostdusty/gobalt"
 )
 
 var keyboard = tgbotapi.NewInlineKeyboardMarkup(
@@ -19,51 +18,58 @@ var keyboard = tgbotapi.NewInlineKeyboardMarkup(
 
 func main() {
 	godotenv.Load()
+	tgbotapi.SetLogger(NewLogger())
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tgbotapi.SetLogger(NewLogger())
-	bot.Debug = true // Enable debug mode
+	bot.Debug = true
+	log.Infof("Authorized on account %s", bot.Self.UserName)
 	updateConfig := tgbotapi.NewUpdate(0)
 	updateConfig.Timeout = 30
 	updates := bot.GetUpdatesChan(updateConfig)
 
 	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
+		if update.Message != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Select a download option")
+			msg.ReplyToMessageID = update.Message.MessageID
 
-		// Take the Chat ID and Text from the incoming message
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		// Check if the message *is* a valid URL and grab it
-		// Example of valid messages:
-		// `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
-		u, err := url.ParseRequestURI(update.Message.Text)
-		if err != nil {
-			msg.Text += "Send a valid URL pls"
-		} else {
-			downloadMedia := gobalt.CreateDefaultSettings()
-			downloadMedia.Url = u.String()
-			result, err := gobalt.Run(downloadMedia)
+			// Check if the message *is* a valid URL and grab it
+			u, err := url.ParseRequestURI(update.Message.Text)
+			log.Info("url.ParseRequestURI", "url", u)
 			if err != nil {
-				log.Error(err)
-				msg.Text += "Oops, something went wrong"
-				msg.Text += "\n" + err.Error()
+				msg.Text += "Send a valid URL pls"
 			} else {
-				// Return the url from cobalt to download the requested media.
-				// result.URL -> https://us4-co.wuk.sh/api/stream?t=wTn-71aaWAcV2RBejNFN.....
-				msg.Text += "status: " + result.Text
-				msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
-					tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonURL("Download here", result.URL)))
+				msg.ReplyMarkup = keyboard
+				// downloadMedia := gobalt.CreateDefaultSettings()
+				// downloadMedia.Url = u.String()
+				// result, err := gobalt.Run(downloadMedia)
+				// if err != nil {
+				// 	log.Error(err)
+				// 	msg.Text += "Oops, something went wrong"
+				// 	msg.Text += "\n" + err.Error()
+				// } else {
+				// 	// Return the url from cobalt to download the requested media.
+				// 	// result.URL -> https://us4-co.wuk.sh/api/stream?t=wTn-71aaWAcV2RBejNFN.....
+				// 	msg.Text += "status: " + result.Text
+				// 	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+				// 		tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonURL("Download here", result.URL)))
+				// }
 			}
-		}
+			if _, err := bot.Send(msg); err != nil {
+				log.Fatal(err)
+			}
+		} else if update.CallbackQuery != nil {
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+			if _, err := bot.Request(callback); err != nil {
+				log.Fatal(err)
+			}
 
-		if _, err := bot.Send(msg); err != nil {
-			log.Fatal(err)
+			msg := tgbotapi.NewEditMessageText(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID, callback.Text)
+			if _, err := bot.Send(msg); err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 }
